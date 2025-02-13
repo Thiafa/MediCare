38d8f4b1cc797140caa661a12f8a38fedb2aed7a
Thiafa <thiafa.vos@gmail.com>
2025-02-12 23:15:46 -0300

    feat: configurando template admin-lte

---
+++ resources/views/layouts/_partials/navbar.blade.php
@@ -0,0 +1,56 @@
+<nav class="app-header navbar navbar-expand bg-body">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item"> <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                    <i class="fa-solid fa-bars"></i> </a> </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown">
+                    <span class="d-none d-md-inline">User</span> </a>
+                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
+                    <li class="user-footer">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="#" class="btn btn-default btn-flat">{{ __('Profile') }}</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href=""
+                                    onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                                    <i class="mr-2"></i>{{ __('Sign Out') }}
+                                </a>
+                                <form id="logout-form" action="" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            {{-- Theme --}}
+            <li class="nav-item dropdown"> <button
+                    class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center"
+                    id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown"
+                    data-bs-display="static" aria-label="Toggle theme (dark)"> <span class="theme-icon-active">
+                        <i class="bi bi-moon-fill me-2"></i> </span> <span class="d-lg-none ms-2"
+                        id="bd-theme-text">Toggle theme</span> </button>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text"
+                    style="--bs-dropdown-min-width: 8rem;">
+                    <li> <button type="button" class="dropdown-item d-flex align-items-center"
+                            data-bs-theme-value="light" aria-pressed="false"> <i class="me-1 fa-solid fa-sun"></i>
+                            Light
+                            <i class="bi bi-check-lg ms-auto d-none"></i> </button> </li>
+                    <li> <button type="button" class="dropdown-item d-flex align-items-center active"
+                            data-bs-theme-value="dark" aria-pressed="true"> <i class="me-1 fa-solid fa-moon"></i>
+                            Dark
+                            <i class="bi bi-check-lg ms-auto d-none"></i> </button> </li>
+                    <li> <button type="button" class="dropdown-item d-flex align-items-center"
+                            data-bs-theme-value="auto" aria-pressed="false">
+                            <i class="me-1 fa-solid fa-desktop"></i>
+                            Auto </button> </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/layouts/_partials/sidebar.blade.php
@@ -0,0 +1,31 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <div class="sidebar-brand"> <a class="brand-link" href="/dist/pages/">
+            <span class="brand-text fw-light">{{ env('APP_NAME') }}</span>
+
+        </a>
+
+    </div>
+    <div class="sidebar-wrapper" data-overlayscrollbars="host">
+        <div class="os-size-observer os-size-observer-appear">
+            <div class="os-size-observer-listener ltr"></div>
+        </div>
+        <div>
+            <nav class="mt-2">
+                <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu"
+                    data-accordion="false">
+                    <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-speedometer"></i>
+                            <p>
+                                Dashboard
+                                <i class="nav-arrow bi bi-chevron-right"></i>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item"> <a href="#" class="nav-link"> <i
+                                class="nav-icon bi bi-circle text-info"></i>
+                            <p>Informational</p>
+                        </a> </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</aside>
