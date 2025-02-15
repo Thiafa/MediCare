58d95a0c29d0377c1ec9af3ae53c8eb9aaba845f
Thiafa <thiafa.vos@gmail.com>
2025-02-15 16:37:52 -0300

    feat: adicionar modelo de Medico, migração e popular com dados de exemplo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            PacienteSeeder::class
+            PacienteSeeder::class,
+            MedicoSeeder::class
         ]);
     }
 }
